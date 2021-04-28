8ee9dd5b8a8f5ae3c58b8fcc07c85cab2e7bc0c0
Rajaomaria Jaona <rajaomariajaona11@gmail.com>
2021-04-28 11:43:34 +0300

    feat: color by timeline node

--- timeline.js
+++ timeline.js
@@ -1,25 +1,29 @@
 function timeline() {
     const dataTimeline = [{
         title: "Parcours 0",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?"
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?",
     }, {
         title: "Parcours 1",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?"
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?",
     }, {
         title: "Parcours 2",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?"
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?",
     }, {
         title: "Parcours 3",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?"
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?",
     }, {
         title: "Parcours 4",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?"
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?",
     }, {
         title: "Parcours 5",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?"
-    }, ]
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est repellat nam labore dolor eos odit alias aliquam magnam deserunt asperiores at nisi, quasi facere mollitia ad quo delectus. Blanditiis?",
+    }, ];
 
-    const foreign = d3.select("g#menu-parcours").select("svg")
+    const color = d3.scaleOrdinal(d3.schemeTableau10);
+
+    const foreign = d3
+        .select("g#menu-parcours")
+        .select("svg")
         .style("overflow", "visible")
         .select("g")
         .append("foreignObject")
@@ -27,30 +31,38 @@ function timeline() {
         .attr("y", 0)
         .attr("width", width)
         .attr("height", height)
-        .style("overflow", "visible")
-    const container = foreign.append("xhtml:div")
-        .classed("container", true)
-    const ul_timeline = container.append("xhtml:ul")
+        .style("overflow", "visible");
+    const container = foreign.append("xhtml:div").classed("container", true);
+    const ul_timeline = container
+        .append("xhtml:ul")
         .classed("timeline", true)
         .selectAll("li")
         .data(dataTimeline)
-        .enter()
-    const li_timeline = ul_timeline.append("xhtml:li")
-        .classed("timeline-inverted", (d, i) => i % 2 == 1)
+        .enter();
+    const li_timeline = ul_timeline
+        .append("xhtml:li")
+        .classed("timeline-inverted", (d, i) => i % 2 == 1);
 
-    li_timeline.append("xhtml:div")
+    li_timeline
+        .append("xhtml:div")
         .classed("timeline-badge primary", true)
         .append("xhtml:a")
         .append("xhtml:i")
         .attr("class", "fa fa-dot-circle-o")
-    const panel_timeline = li_timeline.append("xhtml:div")
-        .classed("timeline-panel", true)
-    const head_timeline = panel_timeline.append("xhtml:div")
-        .classed("timeline-heading", true)
-    head_timeline.append("xhtml:h3")
-        .text(d => d.title)
-    const body_timeline = panel_timeline.append("xhtml:div")
+        .style("color", (d, i) => (d.color = color(i)));
+    const panel_timeline = li_timeline
+        .append("xhtml:div")
+        .classed("timeline-panel", true);
+    const head_timeline = panel_timeline
+        .append("xhtml:div")
+        .classed("timeline-heading", true);
+    head_timeline
+        .append("xhtml:h3")
+        .style("color", d => d.color)
+        .text((d) => d.title);
+    const body_timeline = panel_timeline
+        .append("xhtml:div")
         .classed("timeline-body", true)
         .append("xhtml:p")
-        .text(d => d.description)
+        .text((d) => d.description);
 }
