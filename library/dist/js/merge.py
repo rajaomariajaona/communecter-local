84ad62d82db18080be2772c0d2325be6df49c0ea
Rajaomaria Jaona <rajaomariajaona11@gmail.com>
2021-05-20 09:32:24 +0300

    add mindmap graph to merger

--- library/dist/js/merge.py
+++ library/dist/js/merge.py
@@ -1,5 +1,5 @@
 out = "all.js"
-files = ["graph.js","graph-utils.js","graph-tooltip.js", "circle-graph.js","relation-graph.js"]
+files = ["graph.js","graph-utils.js","graph-tooltip.js", "circle-graph.js","relation-graph.js", "mindmap-graph.js"]
 with open(out, 'w') as outfile:
     for fname in files:
         with open(fname) as infile:
