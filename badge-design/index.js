c379efd6e75b75b024c561231a0c21c6a4106f61
Rajaomaria Jaona <rajaomariajaona11@gmail.com>
2021-09-10 15:08:34 +0300

    command

--- badge-design/index.js
+++ badge-design/index.js
@@ -45,11 +45,21 @@ class SvgUtils {
     }
 
     static getMousePosition(event) {
-        var CTM = artboard.getScreenCTM();
+        var CTM = Artboard.getInstance().artboard.getScreenCTM();
         return {
           x: (event.clientX - CTM.e) / CTM.a,
           y: (event.clientY - CTM.f) / CTM.d
         };
+    }
+    static insertAtIndex(element, index){
+        const artboard = Artboard.getInstance().artboard;
+        if(index < 0){
+            throw 'Negative Index';
+        }else if(index >= artboard.childElementCount){
+            artboard.appendChild(element);
+        }else{
+            artboard.insertBefore(artboard.children[index + 1], element);
+        }
     }
 }
 
@@ -156,25 +166,72 @@ class DragCommand extends Command {
             SvgUtils.setPoint(this._element, this._start);
         }
     }
+    static attachDragEvent(element){
+        element.removeEventListener("mousedown", DragCommand.dragStart);
+        element.removeEventListener("mouseup", DragCommand.dragEnd);
+        element.addEventListener("mousedown", DragCommand.dragStart);
+        element.addEventListener("mouseup", DragCommand.dragEnd);
+    }
+    static dragStart(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const target = event.currentTarget;
+        CurrentElement.selectedElement = target;
+        var x = target.getAttribute('x');
+        x = x ? parseFloat(x) : 0;
+        var y = target.getAttribute('y');
+        y = y ? parseFloat(y) : 0;
+
+        startDragPoint = new Point(x,y);
+
+        CurrentElement.offset = SvgUtils.getMousePosition(event);
+        CurrentElement.offset.x -= x;
+        CurrentElement.offset.y -= y;
+        document.addEventListener("mouseup", DragCommand.dragEnd);
+        document.addEventListener("mousemove", DragCommand.drag);
+    }
+    
+    static dragEnd(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        document.removeEventListener("mouseup", DragCommand.dragEnd);
+        document.removeEventListener("mousemove", DragCommand.drag);
+        if(CurrentElement.isDragging){
+            CurrentElement.isDragging = false;
+            const {x,y} = SvgUtils.getRectOfElement(CurrentElement.selectedElement).toObject();
+            stackControl.do(new DragCommand(CurrentElement.selectedElement, startDragPoint, new Point(x,y)));
+            console.log("DRAGEND");
+        }
+    }
+    static drag(event){
+        if(!CurrentElement.isDragging){
+            console.log("DRAGSTART");
+            CurrentElement.isDragging = true;
+        }
+        if(CurrentElement.isDragging){
+            event.preventDefault();
+            event.stopPropagation();
+            var coord = SvgUtils.getMousePosition(event);
+            SvgUtils.setPoint(CurrentElement.selectedElement, new Point(coord.x - CurrentElement.offset.x, coord.y - CurrentElement.offset.y));
+        }
+    }
 }
 window.DragCommand = DragCommand;
 
 class DuplicateCommand extends Command{
     _element;
     _duplicatedElement;
-    _artboard;
-    constructor(element, artboard){
+    constructor(element){
         super();
-        this._artboard = artboard;
         this._element = element;
         this._duplicatedElement = element.cloneNode(true);
     }
     execute(){
-        var index = Array.from(this._artboard.children).indexOf(this._element);
-        if(index != this._artboard.childElementCount - 1){
-            this._artboard.insertBefore(this._duplicatedElement, this._artboard.children[index + 1]);
+        var index = Array.from(Artboard.getInstance().artboard.children).indexOf(this._element);
+        if(index != Artboard.getInstance().artboard.childElementCount - 1){
+            Artboard.getInstance().artboard.insertBefore(this._duplicatedElement, Artboard.getInstance().artboard.children[index + 1]);
         }else{
-            this._artboard.appendChild(this._duplicatedElement);
+            Artboard.getInstance().artboard.appendChild(this._duplicatedElement);
         }
         var {x,y} = SvgUtils.getRectOfElement(this._element).toObject();
         x += 100;
@@ -183,34 +240,153 @@ class DuplicateCommand extends Command{
         CurrentElement.selectedElement = this._duplicatedElement;
     }
     revert(){
-        this._artboard.removeChild(this._duplicatedElement);
+        Artboard.getInstance().artboard.removeChild(this._duplicatedElement);
         if(CurrentElement.selectedElement == this._duplicatedElement){
-            Handler.getInstance().hide();
+            CurrentElement.selectedElement = null;
         }
     }
 }
 window.DuplicateCommand = DuplicateCommand;
 
+class DeleteCommand extends Command{
+    _element;
+    _position = 0;
+    constructor(element){
+        super();
+        this._element = element;
+    }
+    execute(){
+        this._position = Array.from(Artboard.getInstance().artboard.children).indexOf(this._element);
+        if(this._position > 0){
+            Artboard.getInstance().artboard.removeChild(this._element);
+            if(CurrentElement.selectedElement == this._element){
+                CurrentElement.selectedElement = null;
+            }
+        }
+    }
+    revert(){
+        SvgUtils.insertAtIndex(this._element, this._position);
+    }
+
+}
+
+window.DeleteCommand = DeleteCommand;
+
+class LayerCommand extends Command{
+    _possibleRequest = ['down', 'up'];
+    _element;
+    _request;
+    constructor(element, request){
+        super();
+        if(!this._possibleRequest.includes(request)){
+            throw 'Command ' + request + ' impossible';
+        }
+        this._element = element;
+        this._request = request;
+    }
+    execute(){
+        switch (this._request) {
+            case 'up':
+                this.putUp();
+                break;
+            case 'down':
+                this.putDown();
+                break;
+            default:
+                break;
+        }
+    }
+    revert(){
+        switch (this._request) {
+            case 'down':
+                this.putUp();
+                break;
+            case 'up':
+                this.putDown();
+                break;
+            default:
+                break;
+        }
+    }
+    putDown(){
+        if(this._element){
+            if(this.isActiveDown()){
+                var index = Array.from(Artboard.getInstance().artboard.children).indexOf(this._element);
+                Artboard.getInstance().artboard.insertBefore(Artboard.getInstance().artboard.children[index], Artboard.getInstance().artboard.children[index - 1])
+            }
+        }
+    }
+    putUp(){
+        if(this._element){
+            if(this.isActiveUp()){
+                var index = Array.from(Artboard.getInstance().artboard.children).indexOf(this._element);
+                Artboard.getInstance().artboard.insertBefore(Artboard.getInstance().artboard.children[index + 1], Artboard.getInstance().artboard.children[index])
+            }
+        }
+    }
+
+    isActiveUp(){
+        if(this._element){
+            var index = Array.from(Artboard.getInstance().artboard.children).indexOf(this._element);
+            return index + 1 < Artboard.getInstance().artboard.childElementCount;
+        }
+    }
+
+    isActiveDown(){
+        if(this._element){
+            var index = Array.from(Artboard.getInstance().artboard.children).indexOf(this._element);
+            return index > 0 && index < Artboard.getInstance().artboard.childElementCount;
+        }
+    }
+}
+window.LayerCommand = LayerCommand;
 
 
 class CurrentElement{
     static _selectedElement;
+    static _isDragging;
+    static _offset;
     static get selectedElement() {
         return this._selectedElement;
     }
     static set selectedElement(_selectedElement) {
         if(this._selectedElement){
-            this._selectedElement.removeEventListener('handler', (event) => Handler.getInstance().put(event));
+            this._selectedElement.removeEventListener('handler', Handler.getInstance().put);
         }
         this._selectedElement = _selectedElement;
         if(this._selectedElement){
-            this._selectedElement.addEventListener('handler', (event) => Handler.getInstance().put(event));
-            Handler.getInstance().dispatchHandler(this._selectedElement)
+            CustomElement.init(this._selectedElement, true);
+        }else{
+            Handler.getInstance().hide();
         }
+    }
+    static get isDragging () {
+        return this._isDragging;
+    }
+    static set isDragging (_isDragging) {
+        this._isDragging = _isDragging;
+    }
+    static get offset () {
+        return this._offset;
+    }
+    static set offset (_offset) {
+        this._offset = _offset;
     }
 }
 
 window.CurrentElement = CurrentElement;
+
+class CustomElement{
+    static init(element, selectioned = false){
+        element.addEventListener('handler', Handler.getInstance().put);
+        DragCommand.attachDragEvent(element);
+        if(selectioned){
+            Handler.getInstance().dispatchHandler(element);
+        }
+    }
+}
+
+window.CustomElement = CustomElement;
 
 class Handler{
     _instance = null;
@@ -234,21 +410,22 @@ class Handler{
         return this._instance;
     }
     _init(){
-        this._handleTL.addEventListener('mousedown', function(event) { document.addEventListener('mouseup', function(event) {   artboard.removeEventListener('mousemove', Handler.getInstance().scaleTL);}); artboard.addEventListener('mousemove', Handler.getInstance().scaleTL);})
-        this._handleTR.addEventListener('mousedown', function(event) { document.addEventListener('mouseup', function(event) {   artboard.removeEventListener('mousemove', Handler.getInstance().scaleTR);}); artboard.addEventListener('mousemove', Handler.getInstance().scaleTR);})
-        this._handleBR.addEventListener('mousedown', function(event) { document.addEventListener('mouseup', function(event) {   artboard.removeEventListener('mousemove', Handler.getInstance().scaleBR);}); artboard.addEventListener('mousemove', Handler.getInstance().scaleBR);})
-        this._handleBL.addEventListener('mousedown', function(event) { document.addEventListener('mouseup', function(event) {   artboard.removeEventListener('mousemove', Handler.getInstance().scaleBL);}); artboard.addEventListener('mousemove', Handler.getInstance().scaleBL);})
+        this._handleTL.addEventListener('mousedown', function(event) { document.addEventListener('mouseup', function(event) {   Artboard.getInstance().artboard.removeEventListener('mousemove', Handler.getInstance().scaleTL);}); Artboard.getInstance().artboard.addEventListener('mousemove', Handler.getInstance().scaleTL);})
+        this._handleTR.addEventListener('mousedown', function(event) { document.addEventListener('mouseup', function(event) {   Artboard.getInstance().artboard.removeEventListener('mousemove', Handler.getInstance().scaleTR);}); Artboard.getInstance().artboard.addEventListener('mousemove', Handler.getInstance().scaleTR);})
+        this._handleBR.addEventListener('mousedown', function(event) { document.addEventListener('mouseup', function(event) {   Artboard.getInstance().artboard.removeEventListener('mousemove', Handler.getInstance().scaleBR);}); Artboard.getInstance().artboard.addEventListener('mousemove', Handler.getInstance().scaleBR);})
+        this._handleBL.addEventListener('mousedown', function(event) { document.addEventListener('mouseup', function(event) {   Artboard.getInstance().artboard.removeEventListener('mousemove', Handler.getInstance().scaleBL);}); Artboard.getInstance().artboard.addEventListener('mousemove', Handler.getInstance().scaleBL);})
     }
     dispatchHandler(element){
         element.dispatchEvent(new CustomEvent('handler', {detail : {rectangle : SvgUtils.getRectOfElement(element)}}));
     }
     put(event){
         var {x,y,width, height} = event.detail.rectangle.toObject();
-        this._handler.style.left = x + "px";
-        this._handler.style.top = y + "px";
-        this._handler.style.width = width + "px";
-        this._handler.style.height = height + "px";
-        this._handler.style.display = "block";
+        const handler = document.querySelector("#handler");
+        handler.style.left = x + "px";
+        handler.style.top = y + "px";
+        handler.style.width = width + "px";
+        handler.style.height = height + "px";
+        handler.style.display = "block";
     }
     hide(){
         this._handler.style.display = "none";
@@ -310,3 +487,46 @@ class Handler{
 }
 
 window.Handler = Handler;
+
+class Artboard{
+    _instance = null;
+    _artboard = null;
+    static getInstance(){
+        if(!this._instance){
+            this._instance = new Artboard();
+            this._instance._init();
+        }
+        return this._instance;
+    }
+    
+    _init(){
+        this._artboard = document.querySelector("svg#artboard");
+        this._artboard.addEventListener('click', function(event) {
+            if(!CurrentElement.isDragging && event.currentTarget == event.target){
+                CurrentElement.selectedElement = null;
+            }
+        })
+        this._attachKeyboardEvents();
+        this._attachElementEvents();
+    }
+    _attachKeyboardEvents(){
+        document.addEventListener('keyup', function(event){
+            if(CurrentElement.selectedElement){
+                if(event.which == 46){
+                    remove();
+                }
+            }
+        });
+    }
+    _attachElementEvents(){
+        const elements = this._artboard.querySelectorAll("svg.element");
+        for(const element of elements){
+            CustomElement.init(element);
+        }
+    }
+
+    get artboard () {
+        return this._artboard;
+    }
+
+}
