bf083607dd005cf654483099a590ba93a48d644b
Rajaomaria Jaona <rajaomariajaona11@gmail.com>
2021-12-09 10:42:28 +0300

    merge

--- merge.sh
+++ merge.sh
@@ -1,4 +1,4 @@
 cd library/dist/js
 python3 merge.py
-cp all.js /home/snowden/dev/communecter-php74/modules/graph/assets/js/graph.js
-rm -rf /home/snowden/dev/communecter-php74/pixelhumain/ph/assets/*
+cp all.js /home/snowden/dev/communecter/modules/graph/assets/js/graph.js
+rm -rf /home/snowden/dev/communecter/pixelhumain/ph/assets/*
