635e308270bf4705eb7c4d6888d970b972c65769
Rajaomaria Jaona <rajaomariajaona11@gmail.com>
2022-01-17 12:45:37 +0300

    synchro css

--- merge.sh
+++ merge.sh
@@ -1,4 +1,6 @@
 cd library/dist/js
 python3 merge.py
 cp all.js /home/snowden/dev/communecter/modules/graph/assets/js/graph.js
+cd ../css
+cp graph.css /home/snowden/dev/communecter/modules/graph/assets/css/graph.css
 rm -rf /home/snowden/dev/communecter/pixelhumain/ph/assets/*
