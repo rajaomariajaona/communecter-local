e2374e1a9e85d52f91eb67c9b96cea7911c9634d
Rajaomaria Jaona <rajaomariajaona11@gmail.com>
2022-03-12 15:07:22 +0300

    install babel

--- merge.sh
+++ merge.sh
@@ -1,7 +1,9 @@
 cp venn.js /home/snowden/dev/communecter/modules/graph/assets/js/venn.js
 cd library/dist/js
 python3 merge.py
-cp all.js /home/snowden/dev/communecter/modules/graph/assets/js/graph.js
+npx babel all.js --out-file all-babel.js
+cp all.js /home/snowden/dev/communecter/modules/graph/assets/js/graph-src.js
+cp all-babel.js /home/snowden/dev/communecter/modules/graph/assets/js/graph.js
 cd ../css
 cp graph.css /home/snowden/dev/communecter/modules/graph/assets/css/graph.css
 rm -rf /home/snowden/dev/communecter/pixelhumain/ph/assets/*
