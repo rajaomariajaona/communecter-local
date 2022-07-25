c6f4a618468ffb62130fc55fa3bb3c72a39be602
rajaomariajaona <rajaomariajaona11@gmail.com>
2022-07-25 15:18:51 +0300

    parcours editor

--- library/dist/js/badge-graph.js
+++ library/dist/js/badge-graph.js
@@ -472,7 +472,7 @@ class BadgeGraph extends Graph {
                         var o = {
                             x: this._source.x,
                             y: this._source.y,
-                        };
+S                        };
                         return this._diagonal(o, o);
                     })
                     .style("stroke-width", 1)
