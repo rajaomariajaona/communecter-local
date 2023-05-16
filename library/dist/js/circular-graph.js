5274d75c34948f151165b8a6a6795c4b3583046e
Rajaomaria Jaona <rajaomariajaona11@gmail.com>
2023-05-16 12:06:52 +0300

    circular

--- library/dist/js/circular-graph.js
+++ library/dist/js/circular-graph.js
@@ -4,11 +4,23 @@ class CircularGraph extends Graph{
     _height = 800 - this._margin.top - this._margin.bottom;
     _innerRadius = 0
     _outerRadius = Math.min(this._width, this._height) / 2; 
-    _max = 0;
+    _maxInternal = 0;
+    _maxExternal = 0;
+    _maxInternalMargin = 10;
+    _secondCircleColor = "#00FF28"
     _exteriorColor = "#FF0014"
     _internalBorderColor = "#ffffff"
     _internalBorderWidth = 1
     _internalCircleRadius = 100
+    _secondCircleRadius = 150
+    _barplotCircleRadius = 250
+    _barplotColor = d3.scaleOrdinal([
+        "#F9C1C8",
+        "#DCEEC2",
+        "#FBE5C1",
+        "#B6C5F0",
+        "#CCEFFC",
+    ])
     _defaultColor = d3.scaleOrdinal([
         "#E5E5E5"
     ]);
@@ -51,82 +63,65 @@ class CircularGraph extends Graph{
     }
     preprocessResults(result){
         var result = super.preprocessResults(result);
-        var data = result.internalData;
+        var data = result;
         var maxCount = 0;
-        // for (const [id,row] of Object.entries(result)) {
-        //     if(row.tags){
-        //         for(const tag of row.tags){
-        //             if(this._authorizedTags && Array.isArray(this._authorizedTags) && this._authorizedTags.length > 0){
-        //                 if(this._authorizedTags.includes(tag)){
-        //                     if(!Object.keys(data).includes(tag)){
-        //                         data[tag] = 0;
-        //                     }
-        //                     data[tag]++;
-        //                     if(maxCount < data[tag]){
-        //                         maxCount = data[tag];
-        //                     }
-        //                 }
-        //             }else{
-        //                 if(!Object.keys(data).includes(tag)){
-        //                     data[tag] = 0;
-        //                 }
-        //                 data[tag]++;
-        //                 if(maxCount < data[tag]){
-        //                     maxCount = data[tag];
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-        // if(this._max > 0 && this._max < Object.keys(data).length){
-        //     const entries = Object.entries(data);
-        //     entries.sort((a,b) => b[1] - a[1]);
-        //     const cleanedData = {}
-        //     for (let i = 0; i < this._max; i++) {
-        //         cleanedData[entries[i][0]] = entries[i][1]
-        //     }
-        //     data = cleanedData;
-        // }
-        if(this._max <= 0){
-            for(const [index, values] of Object.entries(data)){
+        if(this._maxInternal <= 0){
+            for(const [index, values] of Object.entries(data.internalData)){
                 for(const value of values){
-                    if(value > this._max){
-                        this._max = value
+                    if(value > this._maxInternal){
+                        this._maxInternal = value
                     }
                 }
             }
         }
+        if(this._maxExternal <= 0){
+            for(const [index, value] of Object.entries(data.externalData)){
+                if(value > this._maxExternal){
+                    this._maxExternal = value
+                }
+            }
+        }
+        this._maxInternal += this._maxInternalMargin
         return data;
     }
-    setMax(value){
-        this._max = isNaN(Number(value)) ? this._max : Number(value);
+    setMaxInternal(value){
+        this._maxInternal = isNaN(Number(value)) ? this._maxInternal : Number(value);
+        this._maxInternal += this._maxInternalMargin
     }
-    _update(data){
+    setMaxExternal(value){
+        this._maxExternal = isNaN(Number(value)) ? this._maxExternal : Number(value);
+    }
+    _updateInternal(data){
         var x = d3.scaleBand()
             .range([0, 2 * Math.PI])
             .align(0)
             .domain(Object.keys(data));
-        var y = d3.scaleRadial()
-            .range([this._innerRadius, this._outerRadius])
-            .domain([0, this._internalCircleRadius]);
+        //SECOND CIRCLE
+        this._rootG.append("g")
+        .classed("second-circle", true)
+        .append("circle")
+        .attr("r", this._secondCircleRadius)
+        .attr("fill", this._secondCircleColor)
+        .style("filter", "drop-shadow(0px 0px 5px rgb(0 0 0 / 0.2))")
 
         //CIRCULAR INTERIOR
         //RED PART
         this._rootG.append("circle")
-            .attr("id", "circle-red-part")
+            .classed("circle-red-part", true)
             .attr("cx", 0)
             .attr("cy", 0)
             .attr("r", this._internalCircleRadius)
             .attr("fill", this._exteriorColor)
+
+            
         // DATA PART
         this._rootG.append("g")
-            .attr("id", "circle-interior")
+            .classed("circle-interior", true)
             .selectAll("g.items")
             .data(Object.entries(data))
             .join((enter) => {
                 const mainG = enter.append("g").classed("items", true)
                 mainG.each(([current, values]) => {
-                    console.log(values)
                     mainG
                         .append("g")
                         .classed("content", true)
@@ -137,7 +132,7 @@ class CircularGraph extends Graph{
                                 .append("path")
                                 .attr("d", d3.arc()     // imagine your doing a part of a donut plot
                                 .innerRadius(this._innerRadius)
-                                .outerRadius((d) => d * this._internalCircleRadius / this._max)
+                                .outerRadius((d) => d * this._internalCircleRadius / this._maxInternal)
                                 .startAngle(function(d, i) { return x(current) + (x.bandwidth() / values.length) * i; })
                                 .endAngle(function(d, i) { return x(current) + (x.bandwidth() / values.length) * (i + 1); })
                                 )
@@ -150,7 +145,7 @@ class CircularGraph extends Graph{
 
         //BORDER
         this._rootG.append("g")
-        .attr("id", "borders")
+        .classed("borders", true)
         .selectAll("g.items")
         .data(Object.entries(data))
         .join((enter) => {
@@ -163,9 +158,43 @@ class CircularGraph extends Graph{
                 .attr("stroke", this._internalBorderColor)
                 .attr("stroke-width", this._internalBorderWidth)
         })
-
-
-
+    }
+    _updateExternal(data){
+        var x = d3.scaleBand()
+            .range([0, 2 * Math.PI])
+            .align(0)
+            .domain(Object.keys(data));
+        var y = d3.scaleRadial()
+            .range([this._secondCircleRadius, this._barplotCircleRadius])
+            .domain([0, this._maxExternal]);
+        this._rootG.append("g")
+            .classed("external", true)
+            .selectAll("g.items")
+            .data(Object.entries(data))
+            .join(enter => {
+                const mainG = enter.append("g").classed("items", true)
+                mainG.append("path")
+                    .attr("fill", this._barplotColor)
+                    .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+                        .innerRadius(this._secondCircleRadius)
+                        .outerRadius(function(d) { return y(d[1]); })
+                        .startAngle(function(d) { return x(d[0]); })
+                        .endAngle(function(d) { return x(d[0]) + x.bandwidth(); })
+                        .padAngle(0.01)
+                        .padRadius(this._secondCircleRadius))
+            mainG.append("g")
+                .attr("text-anchor", function(d) { return (x(d[0]) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+                .attr("transform", function(d) { return "rotate(" + ((x(d[0]) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d[1])+10) + ",0)"; })
+              .append("text")
+                .text(function(d){return(d[0])})
+                .attr("transform", function(d) { return (x(d[0]) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+                .style("font-size", "9px")
+                .attr("alignment-baseline", "middle")
+            })
+    }
+    _update(data){
+        this._updateInternal(data.internalData)
+        this._updateExternal(data.externalData)
         this.initZoom();
     }
 }
