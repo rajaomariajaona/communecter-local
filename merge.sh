fcf93c982c89e008cfe242e70a4c4ae95b53d47d
Rajaomaria Jaona <rajaomariajaona11@gmail.com>
2023-05-24 07:51:04 +0300

    merge fix

--- merge.sh
+++ merge.sh
@@ -1,9 +1,9 @@
-cp venn.js /home/snowden/dev/communecter/modules/graph/assets/js/venn.js
+cp venn.js /home/snowden/dev/communecter/docker/code/modules/graph/assets/js/venn.js
 cd library/dist/js
 python3 merge.py
 pnpm transpile
-cp all.js /home/snowden/dev/communecter/modules/graph/assets/js/graph-src.js
-cp all-compiled.js /home/snowden/dev/communecter/modules/graph/assets/js/graph.js
+cp all.js /home/snowden/dev/communecter/docker/code/modules/graph/assets/js/graph-src.js
+cp all-compiled.js /home/snowden/dev/communecter/docker/code/modules/graph/assets/js/graph.js
 cd ../css
-cp graph.css /home/snowden/dev/communecter/modules/graph/assets/css/graph.css
-rm -rf /home/snowden/dev/communecter/pixelhumain/ph/assets/*
+cp graph.css /home/snowden/dev/communecter/docker/code/modules/graph/assets/css/graph.css
+rm -rf /home/snowden/dev/communecter/docker/code/pixelhumain/ph/assets/*
